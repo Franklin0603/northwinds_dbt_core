bd3707aa9223acd06ee0e10ebca6a712063587f3
Franklin Ajisogun <franklin.ajisogun03@gmail.com>
2024-02-06 13:59:22 -0500

    add int_campanies

---
+++ northwinds_dbt/models/integration/int_campanies.sql
@@ -0,0 +1,57 @@
+WITH hubspot_companies AS (
+    SELECT 
+        company_id,
+        business_name
+    FROM 
+        {{ ref('stg_hubspot_companies') }}
+), 
+rds_companies AS (
+    SELECT 
+        company_id,
+        name,
+        address,
+        postal_code,
+        city, 
+        country  
+    FROM 
+        {{ ref('stg_rds_companies') }}
+), 
+merged_companies as (
+    SELECT 
+        company_id as hubspot_company_id, 
+        null as rds_company_id, 
+        business_name as name 
+    FROM 
+        hubspot_companies 
+    
+    UNION ALL
+    
+    SELECT 
+        NULL as hubspot_company_id, 
+        company_id as rds_company_id, 
+        name
+    FROM 
+        rds_companies
+), 
+deduped as (
+    SELECT 
+        max(hubspot_company_id) as hubspot_company_id,
+        max(rds_company_id) as rds_company_id, 
+        name
+     FROM 
+        merged_companies 
+    GROUP BY 
+        name
+)
+SELECT 
+    {{ dbt_utils.generate_surrogate_key(['deduped.name']) }} as company_pk, 
+    deduped.hubspot_company_id, 
+    deduped.rds_company_id, 
+    deduped.name, 
+    rds_companies.address,
+    rds_companies.postal_code, 
+    rds_companies.city, 
+    rds_companies.country 
+FROM deduped
+LEFT OUTER JOIN rds_companies 
+    ON rds_companies.company_id = deduped.rds_company_id
