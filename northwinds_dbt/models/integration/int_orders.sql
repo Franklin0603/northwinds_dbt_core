6b13ada2213037967be733913b692ce72643f165
Franklin Ajisogun <franklin.ajisogun03@gmail.com>
2024-02-07 16:12:55 -0500

    add stg_rds_orders

---
+++ northwinds_dbt/models/integration/int_orders.sql
@@ -0,0 +1,25 @@
+WITH stg_rds_orders AS (
+    SELECT 
+        order_id,
+        employee_id,
+        customer_id,
+        order_date,
+        product_id,
+        quantity, 
+        discount, 
+        unit_price 
+    FROM {{ ref('stg_rds_orders')}}
+), 
+generate_order_id AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['order_id', 'product_id', 'customer_id']) }} as contact_pk, 
+        employee_id,
+        customer_id,
+        order_date,
+        product_id,
+        quantity, 
+        discount, 
+        unit_price 
+    FROM stg_rds_orders
+)
+SELECT * FROM generate_order_id
